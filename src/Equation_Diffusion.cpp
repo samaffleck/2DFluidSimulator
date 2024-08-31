eebcd6b5f0cba2c2f3250957f478ae34f820d33a
sam Affleck <sam.affleck@hotmail.com>
2024-08-31 15:32:50 +0100

    Updated the diffusion equations with a better implementation of boundary conditions. This could still be improved by moving boundary conditions from the IEquation class to specific equations

--- src/Equation_Diffusion.cpp
+++ src/Equation_Diffusion.cpp
@@ -23,28 +23,120 @@ void Equation_Diffusion::initialiseEquation(int numberOfXCells, int numberOfYCel
 
 void Equation_Diffusion::update()
 {
+	// Get constants
 	int nx = m_numberOfXCells;
 	int ny = m_numberOfYCells;
-
 	double dx = p_mesh->getCells()[0][0].dx;
 	double dy = p_mesh->getCells()[0][0].dy;
-	double dx_squared_inv = 1 / (dx * dx);
-	double dy_squared_inv = 1 / (dy * dy);
+	double dxx = 1 / (dx * dx);
+	double dyy = 1 / (dy * dy);
 
-	// Update link coefficients
-	for (int x = 0; x < nx; ++x)
+	// BOTTOM LEFT CORNER
+	int x = 0;
+	int y = 0;
+
+	Ao(x, y) = 3 * dxx + 3 * dyy;
+	Ae(x, y) = -dxx;
+	Aw(x, y) = 0.0;
+	An(x, y) = -dyy;
+	As(x, y) = 0.0;
+	S(x, y) = 2 * dxx * m_solver.leftBoundary + 2 * dyy * m_solver.bottomBoundary;
+
+	// BOTTOM RIGHT CORNER
+	x = nx - 1;
+	y = 0;
+
+	Ao(x, y) = 3 * dxx + 3 * dyy;
+	Ae(x, y) = 0.0;
+	Aw(x, y) = -dxx;
+	An(x, y) = -dyy;
+	As(x, y) = 0.0;
+	S(x, y) = 2 * dxx * m_solver.rightBoundary + 2 * dyy * m_solver.bottomBoundary;
+
+	// TOP LEFT CORNER
+	x = 0;
+	y = ny - 1;
+
+	Ao(x, y) = 3 * dxx + 3 * dyy;
+	Ae(x, y) = -dxx;
+	Aw(x, y) = 0.0;
+	An(x, y) = 0.0;
+	As(x, y) = -dyy;
+	S(x, y) = 2 * dxx * m_solver.leftBoundary + 2 * dyy * m_solver.topBoundary;
+
+	// TOP RIGHT CORNER
+	x = nx - 1;
+	y = ny - 1;
+
+	Ao(x, y) = 3 * dxx + 3 * dyy;
+	Ae(x, y) = 0.0;
+	Aw(x, y) = -dxx;
+	An(x, y) = 0.0;
+	As(x, y) = -dyy;
+	S(x, y) = 2 * dxx * m_solver.rightBoundary + 2 * dyy * m_solver.topBoundary;
+
+	// BOTTOM WALL
+	y = 0;
+	for (x = 1; x < nx - 1; ++x)
 	{
-		for (int y = 0; y < ny; ++y)
+		Ao(x, y) = 2 * dxx + 3 * dyy;
+		Ae(x, y) = -dxx;
+		Aw(x, y) = -dxx;
+		An(x, y) = -dyy;
+		As(x, y) = 0.0;
+		S(x, y) = 2 * dyy * m_solver.bottomBoundary;
+	}
+
+	// TOP WALL 
+	y = ny - 1;
+	for (x = 1; x < nx - 1; ++x)
+	{
+		Ao(x, y) = 2 * dxx + 3 * dyy;
+		Ae(x, y) = -dxx;
+		Aw(x, y) = -dxx;
+		An(x, y) = 0.0;
+		As(x, y) = -dyy;
+		S(x, y) = 2 * dyy * m_solver.topBoundary;
+	}
+
+	// LEFT WALL
+	x = 0;
+	for (y = 1; y < ny - 1; ++y)
+	{
+		Ao(x, y) = 3 * dxx + 2 * dyy;
+		Ae(x, y) = -dxx;
+		Aw(x, y) = 0.0;
+		An(x, y) = -dyy;
+		As(x, y) = -dyy;
+		S(x, y) = 2 * dxx * m_solver.leftBoundary;
+	}
+
+	// RIGHT WALL
+	x = nx - 1;
+	for (y = 1; y < ny - 1; ++y)
+	{
+		Ao(x, y) = 3 * dxx + 2 * dyy;
+		Ae(x, y) = 0.0;
+		Aw(x, y) = -dxx;
+		An(x, y) = -dyy;
+		As(x, y) = -dyy;
+		S(x, y) = 2 * dxx * m_solver.rightBoundary;
+	}
+
+	// INTERIOR CELLS	
+	for (x = 1; x < nx - 1; ++x)
+	{
+		for (y = 1; y < ny - 1; ++y)
 		{
-			Ao(x, y) = 2 * dx_squared_inv + 2 * dy_squared_inv;
-			Ae(x, y) = -dx_squared_inv; //
-			Aw(x, y) = -dx_squared_inv; //
-			An(x, y) = -dy_squared_inv; //
-			As(x, y) = -dy_squared_inv; //
-			S(x, y) = 0.0; //
+			Ao(x, y) = 2 * dxx + 2 * dyy;
+			Ae(x, y) = -dxx; 
+			Aw(x, y) = -dxx; 
+			An(x, y) = -dyy; 
+			As(x, y) = -dyy; 
+			S(x, y) = 0.0; 
 		}
 	}
-	
+
 	// Solve the equation
 	m_solver.solve(v, Ao, Ae, Aw, An, As, S);
 }
